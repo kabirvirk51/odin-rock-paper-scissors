245179a1249a1b9ca1c4f2e686194905c7b3f312
Kabir <kabirvirk51@gmail.com>
2023-03-22 18:15:15 -0400

    Created getComputerChoice function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getComputerChoice() {
 
+    let numChoice = Math.floor(Math.random() * 3 + 1);
+    if (numChoice == 1) {
+        let choice = 'Rock'
+        console.log(choice);
+    } else if (numChoice == 2) {
+        let choice = 'Paper'
+        console.log(choice);
+    } else if (numChoice == 3) {
+        let choice = 'Scissors'
+        console.log(choice);
+    }
 
 }
+
+
+getComputerChoice();
