b824507319dbd79568ecadca4393782cf4c3d230
Kabir <kabirvirk51@gmail.com>
2023-03-22 18:40:26 -0400

    Finalized getComputerChoice function

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ function getComputerChoice() {
     let numChoice = Math.floor(Math.random() * 3 + 1);
     if (numChoice == 1) {
         let choice = 'Rock'
-        console.log(choice);
+        return choice
     } else if (numChoice == 2) {
         let choice = 'Paper'
-        console.log(choice);
+        return choice
     } else if (numChoice == 3) {
         let choice = 'Scissors'
-        console.log(choice);
+        return choice
     }
 
 }
 
 
-getComputerChoice();
+
+
+
+
