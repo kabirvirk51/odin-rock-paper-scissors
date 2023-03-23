79405608439e44be95402c1116f6e7170639f76e
Kabir <kabirvirk51@gmail.com>
2023-03-23 11:38:34 -0400

    Started playRound function, however not close to being finished

--- script.js
+++ script.js
@@ -14,8 +14,30 @@ function getComputerChoice() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
 
+    computerSelection = getComputerChoice()
+    computerSelection = computerSelection.toUpperCase();
+    playerSelection = playerSelection.toUpperCase();
+    if (playerSelection == computerSelection) {
+        return 'It is a tie!'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors'
+    } else if (playerSelection === 'Paper' &&  computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper'
+    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+        return 'You lose! Rock beats Scissors'
+    } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+        return 'You lose! Paper beats Rock'
+    } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+        return 'You lose! Scissors beats Paper'
+    } else {
+        return 'Invalid Choice!'
+    }
 
+}
 
 
 
