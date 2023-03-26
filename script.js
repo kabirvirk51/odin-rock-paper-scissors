00b0c56e187be337b2ca7a505cc11ed43a8d558b
Kabir <kabirvirk51@gmail.com>
2023-03-26 13:27:26 -0400

    Finished playRound function, works as intended

--- script.js
+++ script.js
@@ -16,28 +16,30 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    computerSelection = getComputerChoice()
-    computerSelection = computerSelection.toUpperCase();
-    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
         return 'It is a tie!'
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         return 'You win! Rock beats Scissors'
-    } else if (playerSelection === 'Paper' &&  computerSelection === 'Rock') {
+    } else if (playerSelection == 'paper' &&  computerSelection == 'rock') {
         return 'You win! Paper beats Rock'
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         return 'You win! Scissors beats Paper'
-    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         return 'You lose! Rock beats Scissors'
-    } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         return 'You lose! Paper beats Rock'
-    } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         return 'You lose! Scissors beats Paper'
-    } else {
+    } else if (playerSelection != 'rock' && playerSelection != 'scissors' && playerSelection != 'paper') {
         return 'Invalid Choice!'
     }
 
 }
 
 
+const computerSelection = getComputerChoice();
+const playerSelection = 'paper'
+console.log(playRound(playerSelection, computerSelection));
 
